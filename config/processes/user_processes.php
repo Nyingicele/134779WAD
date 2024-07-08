d1cab9be5aea06d7a91e05143a37c7c587a34eb7
Nyingicele <celestine.nyingi@strathmore.edu>
2024-07-08 11:41:01 +0300

    under userprocesses file I added the sign in and out details

---
+++ config/processes/user_processes.php
@@ -0,0 +1,16 @@
+<?php
+/*******
+ this is a php file to enable users processes. Including signup,signin,edit/update and delete.
+ /*** */
+ //Import the database connection
+ require_once "../config/dbConnect.php";
+ //Signup process
+ if (isset($_POST["signup"])){
+    //Variable declaration
+$fullname = mysqli_real_escape_string($dbConn,$_POST["fullname"]);
+$email = mysqli_real_escape_string($dbConn,$_POST["email"]);
+$username = mysqli_real_escape_string($dbConn,$_POST["username"]);
+$password = mysqli_real_escape_string($dbConn,$_POST["password"]);
+$confirmpassword= mysqli_real_escape_string($dbConn,$_POST["confirmpassword"]);
+$usertype = mysqli_real_escape_string($dbConn,$_POST["usertype"]);
+//Check if the password and confirm-password are similar
