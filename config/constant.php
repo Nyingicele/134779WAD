f780ec22b2ef367acf19ba6a6645894cac7fa964
Nyingicele <celestine.nyingi@strathmore.edu>
2024-07-08 11:32:24 +0300

    db connect file created

--- config/constant.php
+++ config/constant.php
@@ -2,6 +2,6 @@
 // Define database constants
 define('HOSTNAME', 'localhost');
 define('HOSTUSER', 'root');
-define('HOSTPASS', 'mysql'); // replace with password if you have one
+define('HOSTPASS', 'mysql'); // replace with your password if you have one
 define('DBNAME', 'webapp');
 ?>
