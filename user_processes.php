c9c26af01385dcf747a06234f1535720ed3632c7
Celestine Nyingi <107841427+Nyingicele@users.noreply.github.com>
2024-08-09 12:50:39 +0300

    Update user_processes.php

--- user_processes.php
+++ user_processes.php
@@ -1,1 +1,6 @@
-
+?php
+/*******
+ this is a php file to enable users processes. Including signup,signin,edit/update and delete.
+ /*** */
+ //Import the database connection
+ require_once "../config/dbConnect.php";
