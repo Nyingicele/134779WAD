e1a05efe14f86f146dd23a87a9b9fc0348979782
Celestine Nyingi <107841427+Nyingicele@users.noreply.github.com>
2024-08-09 12:45:11 +0300

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -2,3 +2,7 @@
 
 $servername = "localhost"; 
 $username = "root"; 
+$password = "";
+$dbname = "test";
+        // Creates connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
