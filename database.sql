54652faf6703430cb4714a449929cda267ad47d6
Celestine Nyingi <107841427+Nyingicele@users.noreply.github.com>
2024-08-09 12:57:19 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -4,3 +4,10 @@ CREATE DATABASE IF NOT EXISTS webapp;
 USE webapp;
 -- create table
 DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+    userid bigint(11) NOT NULL AUTO_INCREMENT,
+    fullname varchar(50) NOT NULL DEFAULT '',
+    username varchar(50) NOT NULL DEFAULT '',
+    email varchar(50) NOT NULL DEFAULT '',
+    password varchar(50) NOT NULL DEFAULT '',
+    userType varchar(30) NOT NULL DEFAULT '',
