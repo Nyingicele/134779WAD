8fb9c16c906896413b76c83d7b6c4c8e30808d17
Celestine Nyingi <107841427+Nyingicele@users.noreply.github.com>
2024-08-09 12:48:21 +0300

    Update constant.php

--- constant.php
+++ constant.php
@@ -1,3 +1,7 @@
 <?php
 // Define database constants
 define('HOSTNAME', 'localhost');
+define('HOSTUSER', 'root');
+define('HOSTPASS', 'mysql'); // replace with your password if you have one
+define('DBNAME', 'webapp');
+?>
