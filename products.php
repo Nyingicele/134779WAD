333efc7ebe4887b4074013a27db74935f4fa8745
Celestine Nyingi <107841427+Nyingicele@users.noreply.github.com>
2024-08-09 09:54:45 +0300

    Update products.php

--- products.php
+++ products.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tech Solutions Inc. | Products</title>
+    <title>MAGARI HUB | Products</title>
     <link rel="stylesheet" href="style.css/style4.css"> <!-- Link to external CSS -->
     <a href="index.html">HOME</a> | <a href="about.html">ABOUT US</a> | <a href="services.html">SERVICES</a> | <a href="products.html">PRODUCTS</a> | <a href="contact.html">CONTACT US</a> | <a href="team.html">TEAM</a> | <a href="career.html">CAREER</a>
 <!--I linked all my pages after I finished creating each-->
