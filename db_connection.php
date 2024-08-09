7615e1e90e5eb0ba5b23d4039ba915270e3cbdcc
Celestine Nyingi <107841427+Nyingicele@users.noreply.github.com>
2024-08-09 12:45:57 +0300

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -6,3 +6,9 @@ $password = "";
 $dbname = "test";
         // Creates connection
     $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Checks connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
