3d141a597594dc9f796755b361f54358779fb5df
Celestine Nyingi <107841427+Nyingicele@users.noreply.github.com>
2024-08-09 12:30:34 +0300

    Update display.php

--- display.php
+++ display.php
@@ -46,3 +46,15 @@ if(isset($_GET["DelId"])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Display Data</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+</head>
+<body>
+    <div class="container">
+    <button class="btn btn-primary" ><a href="contact.php" class="text-light">Add User</a></button>
+
+    <table class="table">
