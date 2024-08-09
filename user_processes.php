f450a5db87a1d163b30e1fb2ac9ed7988c9818ed
Celestine Nyingi <107841427+Nyingicele@users.noreply.github.com>
2024-08-09 12:52:31 +0300

    Update user_processes.php

--- user_processes.php
+++ user_processes.php
@@ -4,3 +4,12 @@
  /*** */
  //Import the database connection
  require_once "../config/dbConnect.php";
+//Signup process
+ if (isset($_POST["signup"])){
+    //Variable declaration
+$fullname = mysqli_real_escape_string($dbConn,$_POST["fullname"]);
+$email = mysqli_real_escape_string($dbConn,$_POST["email"]);
+$username = mysqli_real_escape_string($dbConn,$_POST["username"]);
+$password = mysqli_real_escape_string($dbConn,$_POST["password"]);
+$confirmpassword= mysqli_real_escape_string($dbConn,$_POST["confirmpassword"]);
+$usertype = mysqli_real_escape_string($dbConn,$_POST["usertype"]);
